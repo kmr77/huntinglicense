17799b5f2af4905ecb3af6a8aa404b91eefe72de
KIMURA MASAYUKI <>
2025-04-08 11:59:43 +0900

    ♻️ ボタン表記変更

--- wp-content/themes/hunting-licence/parts-random-btn.php
+++ wp-content/themes/hunting-licence/parts-random-btn.php
@@ -1,6 +1,6 @@
 <div class="random-btn">
   <p>ランダム表示時はページ更新するタイミングでシャッフルされます。</p>
                <button id="toggle-random" class="edit-btn">
-                   <?php echo (isset($_GET['random']) && $_GET['random'] == '1') ? '通常順に戻す' : 'ランダムに表示'; ?>
+                   <?php echo (isset($_GET['random']) && $_GET['random'] == '1') ? '通常順に切り替える' : 'ランダムに切り替える'; ?>
                </button>
              </div>
