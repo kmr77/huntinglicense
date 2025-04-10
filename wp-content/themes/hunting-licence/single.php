424fe64c5e85d563d7263017e16acf539f3c36c2
KIMURA MASAYUKI <>
2025-04-10 12:49:32 +0900

    ♻️ singleページ編集

--- wp-content/themes/hunting-licence/single.php
+++ wp-content/themes/hunting-licence/single.php
@@ -1,12 +1,53 @@
 <?php get_header(); ?>
     <div class="inner">
-        <?php get_template_part('parts-breadcrumb'); ?>
+    <nav class="breadcrumb">
+        <ul>
+            <li>
+            <a href="<?php echo home_url(); ?>">狩猟免許試験例題集 過去問</a>
+            </li>
+
+            <?php
+            // 投稿に関連付けられているカテゴリを取得
+            $categories = get_the_category();
+            if ( !empty($categories) ) {
+            // 先頭のカテゴリを使う（必要に応じて複数対応も可能）
+            $cat = $categories[0];
+            $cat_link = get_category_link( $cat->term_id );
+            echo '<li><a href="' . esc_url( $cat_link ) . '">' . esc_html( $cat->name ) . '</a></li>';
+            }
+            ?>
+
+            <li>問題番号<?php the_field('no'); ?>：<?php the_title(); ?></li>
+        </ul>
+    </nav>
+
     </div>
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <article>
-            <h2><span class="small">設問No.<?php the_field('no'); ?><?php the_title(); ?></h2>
-            <div><?php the_content(); ?></div>
+            <h2>設問No.<?php the_field('no'); ?>：<?php the_title(); ?></h2>
+            <div class="accordion-inner">
+                <div class="single-contents">
+                    <div>
+                        <dl id="accordion">
+                            <dl>
+                                <dt class="select-dt">
+                                    <ul>
+                                        <li><span class="bold">ア：</span><?php the_field('select_a'); ?></li>
+                                        <li><span class="bold">イ：</span><?php the_field('select_i'); ?></li>
+                                        <li><span class="bold">ウ：</span><?php the_field('select_u'); ?></li>
+                                    </ul>
+                                    <div class="mt-1 mb-1 align-center">
+                                        <button class="answer-btn">答えを開閉</button>
+                                    </div>
+                                </dt>
+                                <dd class="answer-dd">
+                                    <span class="answer">答）<?php the_field('answer'); ?><br>
+                                    <?php the_field('answer_body'); ?></span>
+                                </dd>
+                            </dl>
+                        </dl>
+                    </div>
+                </div>
+            </div>
         </article>
     <?php endwhile; endif; ?>
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
