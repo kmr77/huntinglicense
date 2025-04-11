61effaa8a1e9028d6e9922a038146d26f7c588c5
KIMURA MASAYUKI <>
2025-04-11 11:34:20 +0900

    ♻️ 設問ボタン変更

--- wp-content/themes/hunting-licence/category-type1.php
+++ wp-content/themes/hunting-licence/category-type1.php
@@ -53,8 +53,8 @@
                     <dt>
                         <span class="question">問<?php echo $counter; ?>：<span class="small">No.<?php the_field('no'); ?></span> <?php the_title(); ?></span>
                         <div class="btn-layout">
-                          <button class="open-btn">選択肢を見る</button>
-                          <button class="single-btn"><a href="<?php echo esc_url( get_permalink( get_option( 'page_for_posts' ) ) ); ?>" target="blank">設問へ移動</a></button>
+                            <button class="open-btn">選択肢を見る</button>
+                            <button class="single-btn"><a href="<?php the_permalink(); ?>" target="_blank">設問へ移動</a></button>
                         </div>
                     </dt>
                     <dd>
