4eda8815b8745a69b58e124cae7e27c9e45fb81f
KIMURA MASAYUKI <>
2025-04-14 15:23:32 +0900

    不要部分削除

--- wp-content/themes/hunting-licence/function.php
+++ wp-content/themes/hunting-licence/function.php
@@ -12,28 +12,4 @@ function hunting_licence_enqueue_styles() {
 add_action('wp_enqueue_scripts', 'hunting_licence_enqueue_styles');
 
 add_theme_support( 'title-tag' );
-//投稿ページのタイトルにテキスト追記
-function modify_aioseo_title_with_acf( $title ) {
-    if ( is_singular('post') ) {
-        // 現在の投稿IDを取得
-        $post_id = get_queried_object_id();
-        
-        // ACFからnoフィールドを取得
-        $no = get_field('no', $post_id);
-        
-        // ACFフィールド 'no' が存在すればタイトルを変更
-        if ( $no ) {
-            // 投稿の元のタイトルを取得
-            $original_title = get_the_title($post_id);
-            // 新しいタイトルを作成
-            $title = '問題番号' . $no . '：' . $original_title;
-        }
-    }
-    return $title; // 変更されたタイトルを返す
-}
-add_filter( 'aioseop_title', 'modify_aioseo_title_with_acf' );
-
-
-
-
 ?>
