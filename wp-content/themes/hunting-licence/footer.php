b07caea83fe8455460a588ff400c009260230847
KIMURA MASAYUKI <>
2025-04-17 08:44:24 +0900

    ♻️ フッター変更

--- wp-content/themes/hunting-licence/footer.php
+++ wp-content/themes/hunting-licence/footer.php
@@ -1,29 +1,28 @@
-    <div class="footer-navi">
-        <h3>MENU</h3>
-        <ul>
-            <li><a href="https://www.shuryo-menkyo.com/">狩猟免許試験過去問題集</a></li>
-            <li><a href="https://www.shuryo-menkyo.com/know/">知っておくべきこと</a></li>
-            <li><a href="https://www.shuryo-menkyo.com/application/">狩猟免許受験申請</a></li>
-            <li><a href="https://www.shuryo-menkyo.com/content/">狩猟免許試験の内容と対策</a></li>
-            <li><a href="https://www.shuryo-menkyo.com/registration/">狩猟者登録</a></li>
-            <li><a href="https://www.shuryo-menkyo.com/category/all/">全カテゴリ問題</a></li>
-            <li><a href="https://www.shuryo-menkyo.com/category/laws/">法令問題</a></li>
-            <li><a href="https://www.shuryo-menkyo.com/category/type1/">一種猟銃問題</a></li>
-            <li><a href="https://www.shuryo-menkyo.com/category/type2/">二種猟銃問題</a></li>
-            <li><a href="https://www.shuryo-menkyo.com/category/ami/">網（あみ）猟問題</a></li>
-            <li><a href="https://www.shuryo-menkyo.com/category/wana/">罠（わな）猟問題</a></li>
-            <li><a href="https://www.shuryo-menkyo.com/category/animals/">鳥獣問題</a></li>
-            <li><a href="https://www.shuryo-menkyo.com/category/examination/">猟銃等講習会 考査問題</a></li>
-            <li><a href="https://www.shuryo-menkyo.com/category/numbers/">数字問題</a></li>
-            <li><a href="https://www.shuryo-menkyo.com/tag/license/">狩猟免許試験問題のみ</a></li>
-            <li><a href="https://www.shuryo-menkyo.com/tag/examination/">考査問題のみ</a></li>
-            <li><a href="https://www.shuryo-menkyo.com/contact/">お問い合わせ</a></li>
-        </ul>
-    </div>
 </div>
 <footer id="footer">
     <div class="inner">
-        <div class="copy">©️shuryo-menkyo.com</div>
+        <div class="footer-navi">
+            <ul>
+                <li><a href="https://www.shuryo-menkyo.com/">狩猟免許試験過去問題集</a></li>
+                <li><a href="https://www.shuryo-menkyo.com/know/">知っておくべきこと</a></li>
+                <li><a href="https://www.shuryo-menkyo.com/application/">狩猟免許受験申請</a></li>
+                <li><a href="https://www.shuryo-menkyo.com/content/">狩猟免許試験の内容と対策</a></li>
+                <li><a href="https://www.shuryo-menkyo.com/registration/">狩猟者登録</a></li>
+                <li><a href="https://www.shuryo-menkyo.com/category/all/">全カテゴリ問題</a></li>
+                <li><a href="https://www.shuryo-menkyo.com/category/laws/">法令問題</a></li>
+                <li><a href="https://www.shuryo-menkyo.com/category/type1/">一種猟銃問題</a></li>
+                <li><a href="https://www.shuryo-menkyo.com/category/type2/">二種猟銃問題</a></li>
+                <li><a href="https://www.shuryo-menkyo.com/category/ami/">網（あみ）猟問題</a></li>
+                <li><a href="https://www.shuryo-menkyo.com/category/wana/">罠（わな）猟問題</a></li>
+                <li><a href="https://www.shuryo-menkyo.com/category/animals/">鳥獣問題</a></li>
+                <li><a href="https://www.shuryo-menkyo.com/category/examination/">猟銃等講習会 考査問題</a></li>
+                <li><a href="https://www.shuryo-menkyo.com/category/numbers/">数字問題</a></li>
+                <li><a href="https://www.shuryo-menkyo.com/tag/license/">狩猟免許試験問題のみ</a></li>
+                <li><a href="https://www.shuryo-menkyo.com/tag/examination/">考査問題のみ</a></li>
+                <li><a href="https://www.shuryo-menkyo.com/contact/">お問い合わせ</a></li>
+            </ul>
+        </div>
+        <div class="copy">Copyright shuryo-menkyo.com</div>
     </div>
 </footer>
 <script src="<?php echo get_template_directory_uri() ?>/common.js" type="text/javaScript" charset="utf-8"></script>
