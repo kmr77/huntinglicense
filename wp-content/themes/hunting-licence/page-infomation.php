62e0d8aa626507b43cbb0b2cee1e24f8f6917df2
KIMURA MASAYUKI <>
2025-04-17 16:25:23 +0900

    都道府県を記載

--- wp-content/themes/hunting-licence/page-infomation.php
+++ wp-content/themes/hunting-licence/page-infomation.php
@@ -20,12 +20,55 @@ get_header(); ?>
 		<p>また、試験に関するお問い合わせ先や連絡先も掲載していますので、試験に関する疑問や質問がある場合は、直接各都道府県の担当窓口にお問い合わせください。</p>
 		<div class="contents">
 		<ul class="hunting-license-list">
-			<li><strong>北海道</strong> | <a href="URL" target="_blank">詳細情報</a></li>
-			<li><strong>青森県</strong> | <a href="URL" target="_blank">詳細情報</a></li>
-			<li><strong>東京都</strong> | <a href="URL" target="_blank">詳細情報</a></li>
-			<li><strong>大阪府</strong> | <a href="URL" target="_blank">詳細情報</a></li>
-			<!-- 他の都道府県も同様に追加 -->
+			<li><strong>北海道</strong> | <a href="https://www.pref.hokkaido.lg.jp/ks/skn/syuryo/syuryoumenkyo.html" target="_blank">詳細情報</a></li>
+			<li><strong>青森県</strong> | <a href="https://www.pref.aomori.lg.jp/soshiki/kankyo/shizen/syuryoumenkyo.html" target="_blank">詳細情報</a></li>
+			<li><strong>岩手県</strong> | <a href="https://www.pref.iwate.jp/kurashikankyou/shizen/yasei/shuryou/1073864.html" target="_blank">詳細情報</a></li>
+			<li><strong>秋田県</strong> | <a href="https://www.pref.akita.lg.jp/pages/archive/71737" target="_blank">詳細情報</a></li>
+			<li><strong>山形県</strong> | <a href="https://www.pref.yamagata.jp/050011/kurashi/shizen/seibutsu/about_hunting/hunter_license_test/index.html" target="_blank">詳細情報</a></li>
+			<li><strong>宮城県</strong> | <a href="https://www.pref.miyagi.jp/soshiki/sizenhogo/syuryoumenkyosiken.html" target="_blank">詳細情報</a></li>
+			<li><strong>福島県</strong> | <a href="https://www.pref.fukushima.lg.jp/sec/16035b/r6syuryoumennkyokousinn.html" target="_blank">詳細情報</a></li>
+			<li><strong>茨城県</strong> | <a href="https://www.pref.ibaraki.jp/seikatsukankyo/shizen/shizen/hunting_license_exam/exam.html" target="_blank">詳細情報</a></li>
+			<li><strong>栃木県</strong> | <a href="http://www.pref.tochigi.lg.jp/d04/eco/shizenkankyou/shizen/syuryomenkyo.html" target="_blank">詳細情報</a></li>
+			<li><strong>群馬県</strong> | <a href="https://www.pref.gunma.jp/page/637096.html" target="_blank">詳細情報</a></li>
+			<li><strong>千葉県</strong> | <a href="https://www.pref.chiba.lg.jp/shizen/tetsuzuki/syuryo/siken.html" target="_blank">詳細情報</a></li>
+			<li><strong>埼玉県</strong> | <a href="https://www.pref.saitama.lg.jp/a0508/tyouzyu/syuryomenkyo/shiken.html" target="_blank">詳細情報</a></li>
+			<li><strong>東京都</strong> | <a href="https://www.kankyo.metro.tokyo.lg.jp/nature/animals_plants/birds/hunting_license/test.html" target="_blank">詳細情報</a></li>
+			<li><strong>神奈川県</strong> | <a href="http://www.pref.kanagawa.jp/docs/t4i/cnt/f986/p889835.html" target="_blank">詳細情報</a></li>
+			<li><strong>山梨県</strong> | <a href="https://www.pref.yamanashi.jp/shizen/syuryoumenkyosiken.html" target="_blank">詳細情報</a></li>
+			<li><strong>静岡県</strong> | <a href="https://www.pref.shizuoka.jp/kurashikankyo/shizenkankyo/wild/1017684.html" target="_blank">詳細情報</a></li>
+			<li><strong>長野県</strong> | <a href="https://www.pref.nagano.lg.jp/yasei/sangyo/ringyo/shuryo/oshirase.html" target="_blank">詳細情報</a></li>
+			<li><strong>新潟県</strong> | <a href="https://www.pref.niigata.lg.jp/sec/kankyokikaku/1356779151430.html" target="_blank">詳細情報</a></li>
+			<li><strong>富山県</strong> | <a href="https://www.pref.toyama.jp/1709/kurashi/kankyoushizen/shizen/shuryou/2024shiken.html" target="_blank">詳細情報</a></li>
+			<li><strong>石川県</strong> | <a href="https://www.pref.ishikawa.lg.jp/sizen/syuryou/shuryoumenkyo.html" target="_blank">詳細情報</a></li>
+			<li><strong>福井県</strong> | <a href="https://www.pref.fukui.lg.jp/doc/shizen/r6shiken1_2.html" target="_blank">詳細情報</a></li>
+			<li><strong>愛知県</strong> | <a href="https://www.pref.aichi.jp/soshiki/shizen/shuryoumenkyoshiken2024.html" target="_blank">詳細情報</a></li>
+			<li><strong>岐阜県</strong> | <a href="https://www.pref.gifu.lg.jp/page/17280.html" target="_blank">詳細情報</a></li>
+			<li><strong>三重県</strong> | <a href="https://www.pref.mie.lg.jp/TOPICS/m0025300060.htm" target="_blank">詳細情報</a></li>
+			<li><strong>滋賀県</strong> | <a href="https://www.pref.shiga.lg.jp/ippan/kankyoshizen/shizen/14043.html" target="_blank">詳細情報</a></li>
+			<li><strong>京都府</strong> | <a href="https://www.pref.kyoto.jp/choujyu/shuryou.html" target="_blank">詳細情報</a></li>
+			<li><strong>奈良県</strong> | <a href="http://www.pref.nara.jp/24276.htm" target="_blank">詳細情報</a></li>
+			<li><strong>和歌山県</strong> | <a href="https://www.pref.wakayama.lg.jp/prefg/072000/index.html" target="_blank">詳細情報</a></li>
+			<li><strong>大阪府</strong> | <a href="https://www.pref.osaka.lg.jp/doubutu/yaseidoubutu/r4siken.html" target="_blank">詳細情報</a></li>
+			<li><strong>兵庫県</strong> | <a href="https://web.pref.hyogo.lg.jp/nk27/hw24_000000008.html" target="_blank">詳細情報</a></li>
+			<li><strong>鳥取県</strong> | <a href="https://www.pref.tottori.lg.jp/289600.htm" target="_blank">詳細情報</a></li>
+			<li><strong>岡山県</strong> | <a href="https://www.pref.okayama.jp/page/666064.html" target="_blank">詳細情報</a></li>
+			<li><strong>島根県</strong> | <a href="https://www.pref.shimane.lg.jp/industry/norin/choujyu_taisaku/syuryou.html" target="_blank">詳細情報</a></li>
+			<li><strong>広島県</strong> | <a href="https://www.pref.hiroshima.lg.jp/site/huntinglicense/huntinglicense01.html" target="_blank">詳細情報</a></li>
+			<li><strong>山口県</strong> | <a href="https://www.pref.yamaguchi.lg.jp/cms/a15600/syuryoumennkyo/202104200001.html" target="_blank">詳細情報</a></li>
+			<li><strong>香川県</strong> | <a href="https://www.pref.kagawa.lg.jp/midorihozen/shuryo/shuryo/index.html" target="_blank">詳細情報</a></li>
+			<li><strong>徳島県</strong> | <a href="https://www.pref.tokushima.lg.jp/ippannokata/kurashi/shizen/5046875/" target="_blank">詳細情報</a></li>
+			<li><strong>愛媛県</strong> | <a href="https://www.pref.ehime.jp/page/17940.html" target="_blank">詳細情報</a></li>
+			<li><strong>高知県</strong> | <a href="https://www.pref.kochi.lg.jp/soshiki/070201/2022042600121.html" target="_blank">詳細情報</a></li>
+			<li><strong>福岡県</strong> | <a href="https://www.pref.fukuoka.lg.jp/contents/syuryoushiken.html" target="_blank">詳細情報</a></li>
+			<li><strong>熊本県</strong> | <a href="https://www.pref.kumamoto.jp/soshiki/52/200102.html" target="_blank">詳細情報</a></li>
+			<li><strong>佐賀県</strong> | <a href="https://www.pref.saga.lg.jp/kiji00360440/index.html" target="_blank">詳細情報</a></li>
+			<li><strong>長崎県</strong> | <a href="https://www.pref.nagasaki.jp/object/shikaku-shiken-bosyu/shikaku-shiken/659202.html" target="_blank">詳細情報</a></li>
+			<li><strong>大分県</strong> | <a href="https://www.pref.oita.jp/soshiki/16210/r6-syuryousikenn-nittei.html" target="_blank">詳細情報</a></li>
+			<li><strong>宮崎県</strong> | <a href="https://www.pref.miyazaki.lg.jp/shizen/kense/saiyo-shikaku/20240423085228.html" target="_blank">詳細情報</a></li>
+			<li><strong>鹿児島県</strong> | <a href="http://www.pref.kagoshima.jp/ad04/sangyo-rodo/rinsui/shinrin/syuryo/r4syuryoumenkyosiken.html" target="_blank">詳細情報</a></li>
+			<li><strong>沖縄県</strong> | <a href="https://www.pref.okinawa.jp/site/kankyo/shizen/shuryoumenkyoshiken.html" target="_blank">詳細情報</a></li>
 		</ul>
+
 		</div>
 
 <?php get_footer(); ?>
