da73950871bf49c9523317a7fd334dd427c5c7be
KIMURA MASAYUKI <>
2025-05-14 11:56:50 +0900

    フッター強化

--- wp-content/themes/hunting-licence/footer.php
+++ wp-content/themes/hunting-licence/footer.php
@@ -1,31 +1,31 @@
 </div>
 <footer id="footer">
-    <div class="inner">
-        <div class="footer-navi">
-            <ul>
-                <li><a href="https://www.shuryo-menkyo.com/">狩猟免許試験過去問題集</a></li>
-                <li><a href="https://www.shuryo-menkyo.com/know/">知っておくべきこと</a></li>
-                <li><a href="https://www.shuryo-menkyo.com/application/">狩猟免許受験申請</a></li>
-                <li><a href="https://www.shuryo-menkyo.com/content/">狩猟免許試験の内容と対策</a></li>
-                <li><a href="https://www.shuryo-menkyo.com/registration/">狩猟者登録</a></li>
-                <li><a href="https://www.shuryo-menkyo.com/information/">全国の狩猟者免許試験情報一覧<</a></li>
-                <li><a href="https://www.shuryo-menkyo.com/category/all/">狩猟免許全カテゴリ問題</a></li>
-                <li><a href="https://www.shuryo-menkyo.com/category/laws/">法令問題</a></li>
-                <li><a href="https://www.shuryo-menkyo.com/category/type1/">一種猟銃問題</a></li>
-                <li><a href="https://www.shuryo-menkyo.com/category/type2/">二種猟銃問題</a></li>
-                <li><a href="https://www.shuryo-menkyo.com/category/ami/">網（あみ）猟問題</a></li>
-                <li><a href="https://www.shuryo-menkyo.com/category/wana/">罠（わな）猟問題</a></li>
-                <li><a href="https://www.shuryo-menkyo.com/category/animals/">鳥獣問題</a></li>
-                <li><a href="https://www.shuryo-menkyo.com/category/examination/">猟銃等講習会 考査問題</a></li>
-                <li><a href="https://www.shuryo-menkyo.com/category/numbers/">数字問題</a></li>
-                <li><a href="https://www.shuryo-menkyo.com/tag/license/">狩猟免許試験問題のみ</a></li>
-                <li><a href="https://www.shuryo-menkyo.com/tag/examination/">考査問題のみ</a></li>
-                <li><a href="https://www.shuryo-menkyo.com/contact/">お問い合わせ</a></li>
-            </ul>
-        </div>
-        <div class="copy">Copyright <a href="https://www.shuryo-menkyo.com/">狩猟免許ドリル</a></div>
-    </div>
+  <div class="inner">
+    <nav class="footer-navi" aria-label="フッターナビゲーション">
+      <ul class="footer-links">
+        <li><a href="<?php echo home_url('/'); ?>">🏠 狩猟免許試験過去問題集</a></li>
+        <li><a href="<?php echo home_url('/know/'); ?>">📘 知っておくべきこと</a></li>
+        <li><a href="<?php echo home_url('/application/'); ?>">📝 狩猟免許受験申請</a></li>
+        <li><a href="<?php echo home_url('/content/'); ?>">📚 試験の内容と対策</a></li>
+        <li><a href="<?php echo home_url('/registration/'); ?>">🪪 狩猟者登録</a></li>
+        <li><a href="<?php echo home_url('/information/'); ?>">🌐 全国の試験情報</a></li>
+        <li><a href="<?php echo home_url('/category/all/'); ?>">📂 全カテゴリ問題</a></li>
+        <li><a href="<?php echo home_url('/category/laws/'); ?>">⚖️ 法令問題</a></li>
+        <li><a href="<?php echo home_url('/category/type1/'); ?>">🔫 一種猟銃問題</a></li>
+        <li><a href="<?php echo home_url('/category/type2/'); ?>">🎯 二種猟銃問題</a></li>
+        <li><a href="<?php echo home_url('/category/ami/'); ?>">🕸 網猟問題</a></li>
+        <li><a href="<?php echo home_url('/category/wana/'); ?>">🪤 罠猟問題</a></li>
+        <li><a href="<?php echo home_url('/category/animals/'); ?>">🐗 鳥獣問題</a></li>
+        <li><a href="<?php echo home_url('/category/examination/'); ?>">🧪 講習会考査問題</a></li>
+        <li><a href="<?php echo home_url('/category/numbers/'); ?>">🔢 数字問題</a></li>
+        <li><a href="<?php echo home_url('/tag/license/'); ?>">🎓 試験問題のみ</a></li>
+        <li><a href="<?php echo home_url('/tag/examination/'); ?>">🧾 考査問題のみ</a></li>
+        <li><a href="<?php echo home_url('/contact/'); ?>">📩 お問い合わせ</a></li>
+      </ul>
+    </nav>
+    <div class="copy">Copyright <a href="<?php echo home_url('/'); ?>">狩猟免許ドリル</a></div>
+  </div>
 </footer>
-<script src="<?php echo get_template_directory_uri() ?>/common.js" type="text/javaScript" charset="utf-8"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/common.js" type="text/javascript" charset="utf-8"></script>
 </body>
 </html>
