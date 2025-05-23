6404a3b3f04fbdb44655f863b204b0c47934e74f
KIMURA MASAYUKI <>
2025-05-23 10:28:04 +0900

    リンク追加

--- wp-content/themes/hunting-licence/home.php
+++ wp-content/themes/hunting-licence/home.php
@@ -44,6 +44,7 @@
 			<ul>
 				<li><a href="<?php echo home_url('/study-method/'); ?>">▶ 狩猟免許の勉強方法まとめ</a></li>
 				<li><a href="<?php echo home_url('/license-difference/'); ?>">▶ 狩猟免許と銃所持許可の違いを知る</a></li>
+				<li><a href="<?php echo home_url('/license-types/'); ?>">▶ 狩猟免許の種類と違いを比較する</a></li>
 			</ul>
 			</div>
 			<h2>過去問題集</h2>
