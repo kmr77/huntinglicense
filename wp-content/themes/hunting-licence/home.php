3a6a44b2fc362be91e4ad7f057d515a87e1a8136
KIMURA MASAYUKI <>
2025-05-26 17:05:18 +0900

    ♻️ study-methodのタイトル変更に伴う導線変更

--- wp-content/themes/hunting-licence/home.php
+++ wp-content/themes/hunting-licence/home.php
@@ -42,7 +42,7 @@
 			<h2>初心者向けおすすめコンテンツ</h2>
 			<div class="top-link-box">
 			<ul>
-				<li><a href="<?php echo home_url('/study-method/'); ?>">▶ 狩猟免許の勉強方法まとめ</a></li>
+				<li><a href="<?php echo home_url('/study-method/'); ?>">▶ 最短2週間で合格！ 狩猟免許 独学勉強法ガイド</a></li>
 				<li><a href="<?php echo home_url('/license-difference/'); ?>">▶ 狩猟免許と銃所持許可の違いを知る</a></li>
 				<li><a href="<?php echo home_url('/license-types/'); ?>">▶ 狩猟免許の種類と違いを比較する</a></li>
 				<li><a href="<?php echo home_url('/type1-type2-difference/'); ?>">▶ 第一種銃猟と第二種銃猟免許の違い</a></li>
