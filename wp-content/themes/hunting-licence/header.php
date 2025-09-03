d6ce4ace8dc0a1cc12d7586522475eaeb8021f48
KIMURA MASAYUKI <>
2025-09-03 10:44:52 +0900

    newアイコン設置

--- wp-content/themes/hunting-licence/header.php
+++ wp-content/themes/hunting-licence/header.php
@@ -320,7 +320,12 @@ crossorigin="anonymous"></script>
             <li><a href="<?php echo home_url('/category/animals/'); ?>">鳥獣問題</a></li>
             <li><a href="<?php echo home_url('/category/examination/'); ?>">猟銃等講習会 過去問</a></li>
             <li><a href="<?php echo home_url('/category/numbers/'); ?>">数字問題</a></li>
-            <li><a href="<?php echo home_url('/category/animals-judge/'); ?>">狩猟鳥獣イラスト</a></li>
+            <li>
+              <div class="new-icon">
+                <span><img src="<?php echo get_template_directory_uri(); ?>/img/new12.gif" alt="NEW" width="31" height="15"></span>
+                <span><a href="<?php echo home_url('/category/animals-judge/'); ?>">狩猟鳥獣イラスト</a></span>
+              </div>
+          </li>
           </ul>
         </nav>
       </div>
